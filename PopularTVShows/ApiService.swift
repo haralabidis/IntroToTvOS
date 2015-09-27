077a7c69f295981fa59a1a2d2e4f74547f146e4b
Patrick Haralabidis <haralabidis@gmail.com>
2015-09-27 23:28:01 +1000

    Added ApiService Class

---
+++ PopularTVShows/ApiService.swift
@@ -0,0 +1,9 @@
+//
+//  ApiService.swift
+//  PopularTVShows
+//
+//  Created by Patrick Haralabidis on 27/09/2015.
+//  Copyright © 2015 Patrick Haralabidis. All rights reserved.
+//
+
+import Foundation
